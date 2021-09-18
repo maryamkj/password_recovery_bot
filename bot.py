3bdbd54e867cbc5edb06b39394d95fa7e1d99c46
maryamkj <kashanijoumaryam@gmail.com>
2021-09-18 18:53:00 +0430

    Create start_command function

--- bot.py
+++ bot.py
@@ -1,11 +1,12 @@
 from pyrogram import Client
+from pyrogram import filters
 
 app = Client("MaryamKJ", config_file="config.ini")
 
 
 
-@app.on_message()
-async def test(Client, message):
+@app.on_message(filters.command("start"))
+async def start_command(Client, message):
     await app.send_message(message.chat.id , "Hello, let's get started" )
 
 

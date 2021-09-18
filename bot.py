a718376faeab9d70fc4171949b4d7650c1f6eb7c
maryamkj <kashanijoumaryam@gmail.com>
2021-09-18 19:58:08 +0430

    implement the body of start_command function

--- bot.py
+++ bot.py
@@ -1,5 +1,6 @@
 from pyrogram import Client
 from pyrogram import filters
+from pyrogram.types import *
 
 app = Client("MaryamKJ", config_file="config.ini")
 
@@ -7,7 +8,11 @@ app = Client("MaryamKJ", config_file="config.ini")
 
 @app.on_message(filters.command("start"))
 async def start_command(Client, message):
-    await app.send_message(message.chat.id , "Hello, let's get started" )
+    mark = ReplyKeyboardMarkup([
+        [" ثبت اکانت جدید "," اکانت های قدیمی "]
+    ],resize_keyboard=True, one_time_keyboard=True)
+    
+    await app.send_message(message.chat.id , f"سلام {message.from_user.first_name} عزیز لطفا عملیات خود را انتخاب کنید", reply_markup=mark)
 
 
 app.run()
